9a8c8c8a51c32190352e4c177ce8a78b3ce27c36
Robert Hendricks <rmasonh22@gmail.com>
2024-01-13 20:02:32 -0500

    fix update greeting to work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { useState } from "react";
 
+// more complex update with spread oper 
+
 function App() {
-  const [greeting, setGreeting ] = useState({ greet: "Hello World" });
+  const [greeting, setGreeting ] = useState(
+    { greet: "Hello",
+     place: "World"
+   });
   console.log(greeting, setGreeting);
 
   function updateGreeting() {
-    greeting.greet = "Hello, World wide Web";
-    setGreeting(greeting);
+    setGreeting(prevState => {
+      return { ...prevState, place: "World wide Web" }
+    });
+    //greeting.greet = "Hello, World wide Web";
+    //setGreeting(greeting);
   }
   return (
     <div>
-    <h1>{greeting.greet}</h1>
+    <h1>{greeting.greet}, {greeting.place}</h1>
     <button onClick={updateGreeting}>Update Greeting</button>
 
     </div>
